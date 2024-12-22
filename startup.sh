2f9425843292a1ab59d32920706a9d2696c52770
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-22 18:54:57 +0800

    西元2024年12月22日 (週日) 18時54分57秒 CST

--- startup.sh
+++ startup.sh
@@ -5,8 +5,8 @@ script_dir=$(dirname "$0")
 # Change the current working directory to the script's location
 cd "$script_dir"
 
-touch /tmp/ok.txt
+# touch /tmp/ok.txt
 
-#sudo docker-compose down > /dev/null 2>&1
-#sudo docker-compose up --build -d > /dev/null 2>&1
+sudo docker compose down > /dev/null 2>&1
+sudo docker compose up --build -d > /dev/null 2>&1
 

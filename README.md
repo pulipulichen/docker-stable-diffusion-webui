44842107f8853aac4bec1b2d1896c1a5e2dc6c74
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-22 18:40:22 +0800

    西元2024年12月22日 (週日) 18時40分22秒 CST

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # docker-stable-diffusion-webui
 run stable diffusion with docker
 
+# Autostart
+
+````bash
+crontab -e
+````
+
+Add following setting
+````
+@reboot ~/docker-stable-diffusion-webui/startup.sh
+````
+
+
 # Reference
 - https://github.com/ai-dock/stable-diffusion-webui-forge/tree/main/build
--- startup.sh
+++ startup.sh
@@ -4,29 +4,9 @@ script_dir=$(dirname "$0")
 
 # Change the current working directory to the script's location
 cd "$script_dir"
-rm -rf ./output/tmp/lock*
-# rm -rf ./output/screenshot/*.jpg
-# rm -rf ./output/tmp/*.mp4
 
+touch /tmp/ok.txt
 
-# Backup the original file (optional)
-cp -f Dockerfile.template Dockerfile  # Comment out this line if you don't want a backup
+#sudo docker-compose down > /dev/null 2>&1
+#sudo docker-compose up --build -d > /dev/null 2>&1
 
-
-# Check if today is Sunday
-if [[ $(date +%w) == 0 ]]; then
-  # Get today's date in YYYYMMDD format
-  today=$(date +%Y%m%d)
-
-  # Replace "DATE" with YYYYMMDD in output.sh using sed
-  sed -i "s/DATE/$today/g" Dockerfile
-  
-  # echo "Successfully replaced 'DATE' with $today in output.sh"
-fi
-
-sudo docker-compose down > /dev/null 2>&1
-sudo docker-compose up --build -d > /dev/null 2>&1
-
-# sleep 3600000
-# docker-compose down
-# reboot

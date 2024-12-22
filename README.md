d9fa7b05dd664cc42b95fe429551250bf2bb6a25
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-22 18:37:46 +0800

    西元2024年12月22日 (週日) 18時37分46秒 CST

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-stable-diffusion-webui
 run stable diffusion with docker
+
+# Reference
+- https://github.com/ai-dock/stable-diffusion-webui-forge/tree/main/build
---
+++ mail-clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the current user's username
+USER=$(whoami)
+
+# Specify the mail directory
+MAIL_DIR="/var/mail"
+
+# Path to the current user's mail file
+MAIL_FILE="${MAIL_DIR}/${USER}"
+
+# Check if the mail file exists
+if [[ -f $MAIL_FILE ]]; then
+    > "$MAIL_FILE"  # Truncate the file to remove all mail
+    echo "All mail for user '$USER' has been cleared."
+else
+    echo "No mail found for user '$USER'."
+fi

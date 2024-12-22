a8a91d173dc36508ab6ba26df25d5d9617ce0e16
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-22 18:38:44 +0800

    西元2024年12月22日 (週日) 18時38分44秒 CST

--- startup.sh
+++ startup.sh
@@ -24,8 +24,8 @@ if [[ $(date +%w) == 0 ]]; then
   # echo "Successfully replaced 'DATE' with $today in output.sh"
 fi
 
-docker-compose down > /dev/null 2>&1
-docker-compose up --build -d > /dev/null 2>&1
+sudo docker-compose down > /dev/null 2>&1
+sudo docker-compose up --build -d > /dev/null 2>&1
 
 # sleep 3600000
 # docker-compose down

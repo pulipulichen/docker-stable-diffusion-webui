d99b4b5be2574e2c6622932985c8ab84ae5849e0
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-23 20:56:13 +0800

    西元2024年12月23日 (週一) 20時56分13秒 CST

--- stable-diffusion-webui-forge/forge-src/stable-diffusion-webui/extensions-builtin/sd_forge_controlnet/scripts/controlnet.py
+++ stable-diffusion-webui-forge/forge-src/stable-diffusion-webui/extensions-builtin/sd_forge_controlnet/scripts/controlnet.py
@@ -564,7 +564,9 @@ class ControlNetForForgeOfficial(scripts.Script):
     @torch.no_grad()
     def postprocess_batch_list(self, p, pp, *args, **kwargs):
         for i, unit in enumerate(self.get_enabled_units(args)):
+            print('Processing ' + i + ' units: before')
             self.process_unit_after_every_sampling(p, unit, self.current_params[i], pp, *args, **kwargs)
+            print('Processing ' + i + ' units: after')
         return
 
     def postprocess(self, p, processed, *args):

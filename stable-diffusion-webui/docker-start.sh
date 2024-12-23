9ace782a2dd5ca38cfcfc858d70c7319497cacce
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-23 22:42:20 +0800

    西元2024年12月23日 (週一) 22時42分20秒 CST

--- stable-diffusion-webui/docker-start.sh
+++ stable-diffusion-webui/docker-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -m pip version
+"/app/stable-diffusion-webui/venv/bin/python" -m pip install torch==2.1.2 torchvision==0.16.2 --extra-index-url https://download.pytorch.org/whl/cu121
 
 # /app/webui.sh -f --xformers --listen --enable-insecure-extension-access --api --skip-torch-cuda-test

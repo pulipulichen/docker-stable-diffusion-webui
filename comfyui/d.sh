f16c0da072d304f0da352bf2a8977a5ec9fd2783
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-24 10:54:15 +0800

    西元2024年12月24日 (週二) 10時54分15秒 CST

---
+++ comfyui/d.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+script_dir=$(dirname "$0")
+
+# Change the current working directory to the script's location
+cd "$script_dir"
+
+sudo docker compose down > /dev/null 2>&1
+sudo docker compose up --build -d 2>&1
+
---
+++ comfyui/docker-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# python -m ensurepip --default-pip
+# "/app/stable-diffusion-webui/venv/bin/python" -m ensurepip --default-pip
+# "/app/stable-diffusion-webui/venv/bin/python" -m pip install torch==2.1.2 torchvision==0.16.2 --extra-index-url https://download.pytorch.org/whl/cu121
+
+# /app/webui.sh -f --xformers --listen --enable-insecure-extension-access --api --skip-torch-cuda-test
+
+
+script_dir=$(dirname "$0")
+
+# Change the current working directory to the script's location
+cd "$script_dir"
+
+cd ComfyUI
+
+python main.py
